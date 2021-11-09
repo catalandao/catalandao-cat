8806be4d5aad060179b5c020285d14fcceda3690
Àlex Llaó <alex@moybu.com>
2021-11-09 20:44:34 +0100

    Add i18n to project and Dockerize this project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const nextTranslate = require("next-translate");
+
 module.exports = {
     reactStrictMode: true,
+    swcMinify: true,
+    ...nextTranslate(),
+    images: {
+        formats: ["image/avif", "image/webp"]
+        // domains: ["localhost"],
+    },
+    env: {
+        build: new Date()
+    }
 };
