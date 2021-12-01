9db8492dbdd0d362b8fbd457a73f04180e6730b8
Marc Garcia Sastre <markcial@gmail.com>
2021-12-01 13:41:58 +0100

    Hides bidding price label

--- pages/nft.tsx
+++ pages/nft.tsx
@@ -147,10 +147,12 @@ const Component = () => {
             </header>
             <div className="block lg:hidden">
               <Img className="min-h-2xl my-10" src={Tio1} caption={t('nft:intro.help.log_count', { unit: 1, total: 24 })} />
+              {/*
               <div className="flex flex-col">
                 <span className="text-xl lg:text-xl">{t('nft:intro.help.price', { price: 0.25 })}</span>
-                {/* <span className="text-xl lg:text-xl">{t('nft:intro.help.time_remaining', { h: 23, m: 39, s: 23 })}</span> */}
+                <span className="text-xl lg:text-xl">{t('nft:intro.help.time_remaining', { h: 23, m: 39, s: 23 })}</span>
               </div>
+               */}
             </div>
             <p className="max-w-2xl lg:min-h-80">
               {t('nft:intro.text')}
@@ -165,10 +167,12 @@ const Component = () => {
                   <span className="mx-2">&#x2753;</span>{t('nft:intro.help.faq')}</Link>
               </div>
             </div>
+            {/*
             <div className="flex-col hidden lg:flex">
               <span className="text-xl flex lg:text-xl">{t('nft:intro.help.price', { price: 0.25 })}</span>
-              {/*<span className="text-xl flex lg:text-xl">{t('nft:intro.help.time_remaining', { h: 23, m: 39, s: 23 })}</span>*/}
+              <span className="text-xl flex lg:text-xl">{t('nft:intro.help.time_remaining', { h: 23, m: 39, s: 23 })}</span>
             </div>
+            */}
           </RCol>
           <RCol className="hidden lg:flex">
             <Img className="mx-auto w-128" src={Tio1} caption={t('nft:intro.help.log_count', { unit: 1, total: 24 })} />
