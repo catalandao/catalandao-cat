7781d73198fa710ed949080beab130b95db16122
Àlex Llaó <alex@moybu.com>
2022-09-02 09:55:46 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap" rel="stylesheet" />
       */}
       <link rel="icon" href="/cat-dao.svg" />
-      <script async src="https://tally.so/widgets/embed.js"></script>;
+      <script async src="https://tally.so/widgets/embed.js" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Component {...pageProps} />
